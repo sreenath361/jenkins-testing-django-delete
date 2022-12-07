f1e91219af9106f0d38907b5501cf42380835854
Sreenath Reddy <45588260+sreenath361@users.noreply.github.com>
2022-12-07 21:35:25 +0530

    Update run.sh

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,4 +1,4 @@
 # must run aws configure before, and install both ansible and kubectl
-aws eks --region us-west-1 update-kubeconfig --name Sree_EKS_Cluster &&
+aws eks --region us-east-1 update-kubeconfig --name Sree_EKS_Cluster &&
 ansible-playbook playbook.yml
 
