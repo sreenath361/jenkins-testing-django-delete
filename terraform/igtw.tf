4728a7179b4ff9698a7cfd124db327acd1f6f739
Sreenath Reddy <45588260+sreenath361@users.noreply.github.com>
2022-12-07 21:00:21 +0530

    Delete terraform directory

--- terraform/igtw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.EKS_vpc.id
-
-  tags = {
-    Name = "internet_gateway"
-  }
-}
--- terraform/vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "EKS_vpc" {
-  cidr_block = "172.32.0.0/16"
-
-  tags = {
-    Name = "Django_EKS_VPC"
-  }
-}
