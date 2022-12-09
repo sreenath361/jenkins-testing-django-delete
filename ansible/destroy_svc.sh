8d66327309df6b7fc1ad1fa64956b3994f003ba5
root <root@ip-10-32-12-187.ec2.internal>
2022-12-09 12:36:41 +0000

    Updated content

---
+++ ansible/destroy_svc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+kubectl delete deploy djangonotes
+kubectl delete svc djangonotes
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "EKS_vpc" {
   cidr_block = "172.32.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = "Django_EKS_VPC"
